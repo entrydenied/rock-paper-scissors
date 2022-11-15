73d4c21fd10cf42d9e33e51446ab0d2b6fc079d9
entrydenied <benjamintak17@gmail.com>
2022-11-14 22:13:21 -0800

    Add options, randomElement, & choice

--- custom.js
+++ custom.js
@@ -2,7 +2,12 @@ function playRound(playerSelection, computerSelection) {
 
 }
 
-const choices = ['rock', 'paper', 'scissors'];
+const options = ['rock', 'paper', 'scissors'];
+
+const randomElement = array[Math.floor(Math.random() * array.length)]
+
+let choice = prompt("Please make your selection!")
+
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
