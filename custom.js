aa380830cc4266ff56511b531e9c8dbd7a058d37
entrydenied <benjamintak17@gmail.com>
2022-11-14 21:47:48 -0800

    Add playRound & constants

---
+++ custom.js
@@ -0,0 +1,8 @@
+function playRound(playerSelection, computerSelection) {
+
+}
+
+const choices = ['rock', 'paper', 'scissors'];
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
