ef427c9f554c2a8955fa667b727814460412e723
entrydenied <benjamintak17@gmail.com>
2022-11-16 08:29:02 -0800

    Add commented out function

--- custom.js
+++ custom.js
@@ -11,3 +11,9 @@ let choice = prompt("Please make your selection!")
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
+
+/**
+for (let i = 0; i < 5; i++) {
+
+}
+*/
