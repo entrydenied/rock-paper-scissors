e722958251c7ca27b1c53dc03b4bca50d8d1acc0
entrydenied <benjamintak17@gmail.com>
2022-11-16 09:18:19 -0800

    Add playRound function

--- custom.js
+++ custom.js
@@ -13,8 +13,23 @@ const computerSelection = getComputerChoice();
 
  
 function playRound(playerSelection, computerSelection) {
-    
+    if (playerSelection == getComputerChoice) {
+        console.log("It's a tie!")       
+    } else if (playerSelection == ("rock") && getComputerChoice == scissors) {
+        console.log("You Win! Rock beats Scissors!")
+    } else if (playerSelection == ("rock") && getComputerChoice == paper) {
+        console.log("You Lose! Paper beats Rock!")
+    } else if (playerSelection == ("paper") && getComputerChoice == rock) {
+        console.log("You Win! Paper beats Rock!")
+    } else if (playerSelection == ("paper") && getComputerChoice == scissors) {
+        console.log("You Lose! Scissors beats Paper!")
+    } else if (playerSelection == ("scissors") && getComputerChoice == paper) {
+        console.log("You Win! Scissors beats Paper!")
+    } else if (playerSelection == ("scissors") && getComputerChoice == rock) {
+        console.log("You Lose! Rock beats Scissors!")
+    }
 }
+
 /** 
 let choice = prompt("Please make your selection of rock, paper, or scissors!")
 
