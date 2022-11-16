7d217842476b4ea332b6f219dfff5e50f5a4e07b
entrydenied <benjamintak17@gmail.com>
2022-11-16 09:59:34 -0800

    Remove choice from game

--- custom.js
+++ custom.js
@@ -14,6 +14,36 @@ playerSelection = playerSelection.toLowerCase();
  
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
+        return("It's a tie!")       
+    } else if (playerSelection == ("rock") && computerSelection == "scissors") {
+        return("You Win! Rock beats Scissors!")
+    } else if (playerSelection == ("rock") && computerSelection == "paper") {
+        return("You Lose! Rock loses to Paper!")
+    } else if (playerSelection == ("paper") && computerSelection == "rock") {
+        return("You Win! Paper beats Rock!")
+    } else if (playerSelection == ("paper") && computerSelection == "scissors") {
+        return("You Lose! Paper loses to Scissors!")
+    } else if (playerSelection == ("scissors") && computerSelection == "paper") {
+        return("You Win! Scissors beats Paper!")
+    } else if (playerSelection == ("scissors") && computerSelection == "rock") {
+        return("You Lose! Scissors loses to Paper!")
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
+
+
+function game(playRound) {
+
+}
+/** 
+
+for (let i = 0; i < 5; i++) {
+
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
         console.log("It's a tie!")       
     } else if (playerSelection == ("rock") && computerSelection == "scissors") {
         console.log("You Win! Rock beats Scissors!")
@@ -28,20 +58,5 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection == ("scissors") && computerSelection == "rock") {
         console.log("You Lose! Scissors loses to Paper!")
     }
-}
-
-console.log(playRound(playerSelection, computerSelection));
-
-
-
-// let choice = prompt("Please make your selection of rock, paper, or scissors!")
-
-function game(choice, playRound) {
-
-}
-/** 
-
-for (let i = 0; i < 5; i++) {
-
 }
 */
